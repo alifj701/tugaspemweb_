9660d5bf928921c9de65d3553890f3de24d03b60
Alief Nurfadilah <if23.aliefnurfadilah@mhs.ubpkarawang.ac.id>
2024-11-28 15:19:42 +0700

    pertemuan-5

--- index.php
+++ index.php
@@ -3,33 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <!-- Bootstrap CSS CDN -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <title>Ini Halaman Login</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEJb3QhqLMpG8r+Knujs9xylEIfvaxR28X5kD51zT7uyT2dp70eFjq9XWUt5Q" crossorigin="anonymous">
 </head>
 <body>
     <div class="container mt-5">
-        <h1 class="text-center mb-4">Login</h1>
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <form action="./backend/login.php" method="post">
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control" id="email" name="email" placeholder="Masukkan email anda" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Masukkan password anda" required>
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-block" name="submit">Login</button>
-                </form>
+        <h1 class="mb-4">Halaman Login</h1>
+        <form action="./backend/login.php" method="POST">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" name="email" id="email" class="form-control" placeholder="Email" required>
             </div>
-        </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
+        </form>
     </div>
 
-    <!-- Bootstrap JS and dependencies -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pzjw8f+ua7Kw1TIq0u7AyWzZ6r2Vu4T9zU24p9bMi72Yl67oSwwPT72vQVoZ4tdP" crossorigin="anonymous"></script>
 </body>
 </html>
